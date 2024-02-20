f925de7952f399399317f34b03d9f30fde029dd3
ik_2dm3 <ik_2dm3@H015110.NET>
2024-02-20 09:50:54 +0100

    cambios iconos de diseño

--- app/src/main/java/com/example/tfgtxurdinaga/MainActivity.kt
+++ app/src/main/java/com/example/tfgtxurdinaga/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.tfgtxurdinaga
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val intent = Intent(this, Ejemplos::class.java)
+        startActivity(intent)
+
+        finish()
     }
 }
