6c620040f3c75f5411becbf6506f9d4c3db41114
ik_2dm3 <ik_2dm3@H015110.NET>
2024-02-26 10:29:28 +0100

    ventana de ajustes

---
+++ app/src/main/java/com/example/tfgtxurdinaga/ajustes.kt
@@ -0,0 +1,14 @@
+package com.example.tfgtxurdinaga
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+
+class ajustes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ajustes)
+    }
+
+}
